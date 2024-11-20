90c6da9d5be34d180caf290e529a8c3c2162a588
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-11-20 19:14:56 +0700

    update -> Sorted? yes? no? how?

--- 7-kyu/sorted-yes-no-how.js
+++ 7-kyu/sorted-yes-no-how.js
@@ -1,18 +1,18 @@
 function isSortedAndHow(array) {
-  const isAsc = [];
-  const isDes = [];
+  let isAsc = 0;
+  let isDes = 0;
 
   for (let i = 0; i < array.length; i++) {
     if (i === array.length - 1) break;
     const element = array[i];
     const nextElement = array[i + 1];
 
-    if (element > nextElement) isDes.push("yes");
-    else if (element < nextElement) isAsc.push("yes");
+    if (element > nextElement) isDes++;
+    else if (element < nextElement) isAsc++;
   }
 
-  if (isDes.length > 0 && isAsc.length > 0) return "no";
-  else if (isAsc.length === 0) return "yes, descending";
+  if (isDes > 0 && isAsc > 0) return "no";
+  else if (isAsc === 0) return "yes, descending";
   else return "yes, ascending";
 }
 
